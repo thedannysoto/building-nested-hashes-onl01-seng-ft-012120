9fe2cf58fec62efeb2e745ecc07ed46ff01cb5bd
Danny Soto <87dsoto@gmail.com>
2020-01-23 20:31:05 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,10 +20,10 @@ def bonus
    }
   }
 
-epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
+#epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
 
-epic_tragedy[:montagaue][:hero]["status"] = "dead"
-epic_tragedy[:capulet][:heroine]["status"] = "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   
 
