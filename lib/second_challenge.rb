828c0b1c02fbc6dcb399f66f3de721dbbdb2e678
Danny Soto <87dsoto@gmail.com>
2020-01-23 20:14:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
       }, 
     :capulet => {
       #your key/value pairs here!
@@ -9,3 +12,20 @@ def second_challenge
 
   
 end
+
+
+Fill out the empty hash that is the value of the `:montague` key. There are four keys in this hash:
+
+* `:patriarch`
+* `:matriarch`
+* `:hero`
+* `:hero_friends`
+
+The first three of these keys point to a value of an empty hash. The fourth key `:hero_friends` has a value of an empty array.
+
+Then, do the same for the empty hash that is the value of the `:capulet` key. This time, your keys are:
+
+* `:patriarch`
+* `:matriarch`
+* `:heroine`
+* `:heroine_friends`
