2d29f80c4b816ba68d51d8491ba3e19e53ed3b98
Danny Soto <87dsoto@gmail.com>
2020-01-23 20:22:04 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,7 +17,40 @@ def fourth_challenge
       ]
    }
 }
-
-  
-
 end
+
+The hero's two friends are Benvolio and Mercutio. So, the `:hero_friends` array will contain two hashes. Each of these two hashes have the following three keys:
+
+* `:name`
+* `:age`
+* `:attitude`
+
+The hero's first friend has
+
+* a name of "Benvolio",
+* an age of "17", and
+* an attitude of "worried".
+
+The hero's second friend has
+
+* a name of "Mercutio",
+* an age of "18", and
+* an attitude of "hot-headed".
+
+The heroine's two friends are Steven and Nurse. So, the `:heroine_friends` array will contain two hashes. Each of these two hashes have the following three keys:
+
+* `:name`
+* `:age`
+* `:attitude`
+
+The heroine's first friend has
+
+* a name of "Steven",
+* an age of "30", and
+* an attitude of "confused".
+
+The heroine's second friend has
+
+* a name of "Nurse",
+* an age of "44", and
+* an attitude of "worried".
